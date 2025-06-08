1f83c2167cb6cd0ae71d98085203260dda78bd64
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2025-06-08 21:52:48 +0700

    update: update table Service, TestBooking, TestResult, Feedback, MedicalHistory

--- DataAccess/Entities/MedicalHistory.cs
+++ DataAccess/Entities/MedicalHistory.cs
@@ -1,36 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataAccess.Entities;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Entities
+public class MedicalHistory
 {
-    public class MedicalHistory
-    {
-        [Key]
-        public Guid MedicallHistoryId { get; set; }
-
-        public Guid ServiceId { get; set; }
-
-
-        public Guid UserId { get; set; }
-
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-
-        public Guid? TestResultId { get; set; }
-
-        public User User { get; set; }
-
-        [ForeignKey("ServiceId")]
-        public Service Service { get; set; }
-
-        //[ForeignKey("TestResultId")]
-        //public TestResult TestResult { get; set; }
-
-        public ICollection<TestResult> TestResults { get; set; }
-    }
+    [Key]
+    public Guid MedicalHistoryId { get; set; } 
+    public Guid ServiceId { get; set; }
+    public Guid UserId { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+    [ForeignKey("ServiceId")]
+    public Service Service { get; set; }
+    public ICollection<TestResult> TestResults { get; set; } = new List<TestResult>();
 }
