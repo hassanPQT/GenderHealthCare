e9238330c3a44a45e51129bb99683594504f7a6c
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2025-06-17 10:39:57 +0700

    feat: add StaffConsultant DTO + mapper

--- GenderHealcareSystem/CustomActionFilters/AutoMapperProfile.cs
+++ GenderHealcareSystem/CustomActionFilters/AutoMapperProfile.cs
@@ -9,16 +9,27 @@ namespace GenderHealcareSystem.CustomActionFilters
     {
         public AutoMapperProfile()
         {
+            // Map Service
             CreateMap<Service, ServiceDto>().ReverseMap();
             CreateMap<Service, AddServiceRequest>().ReverseMap();
             CreateMap<Service, UpdateServiceRequest>().ReverseMap();
+
+            // Map Blog
             CreateMap<Blog, BlogDto>()
             .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.User))
             .ReverseMap();
             CreateMap<Blog, AddBlogRequest>().ReverseMap();
             CreateMap<Blog, UpdateBlogRequest>().ReverseMap();
+
+            // Map User
             CreateMap<UpdateUserRequest, User>().ForMember(dest => dest.Birthday,
                 opt => opt.MapFrom(src => src.DateOfBirth));
+
+            // Map Staff, Consultant
+            CreateMap<User, StaffConsultantDto>().ReverseMap();
+            CreateMap<AddStaffConsultantRequest, User>()
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId));
+            CreateMap<User, UpdateStaffConsultantRequest>().ReverseMap();
         }
     }
 }
