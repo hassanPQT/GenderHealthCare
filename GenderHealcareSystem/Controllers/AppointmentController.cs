1fa238999ef6818e827cc276c372c3b39cd92fbc
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2025-07-02 01:55:07 +0700

    feat: GetAllServices + GetServiceById

---
+++ GenderHealcareSystem/Controllers/AppointmentController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using BusinessAccess.Services.Interfaces;
+using DataAccess.Entities;
+using GenderHealcareSystem.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenderHealcareSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentService _service;
+        private readonly IMapper _mapper;
+
+        public AppointmentController(IAppointmentService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAppointments([FromQuery] Guid? customerId, [FromQuery] Guid? consultantId, [FromQuery] DateTime? fromDate, [FromQuery] DateTime? toDate, [FromQuery] TimeSpan? fromHour, [FromQuery] TimeSpan? toHour)
+        {
+            var appointmentDomains = await _service.GetAllAsync(customerId, consultantId, fromDate, toDate, fromHour, toHour);
+
+            var appointmentDtos = _mapper.Map<IEnumerable<AppointmentDto>>(appointmentDomains);
+
+            return Ok(appointmentDtos);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetAppointmentById([FromRoute] Guid id)
+        {
+            var appointmentDomain = await _service.GetByIdAsync(id);
+
+            if (appointmentDomain == null)
+                return BadRequest("Appointment is not existed!");
+
+            var appointmentDto = _mapper.Map<AppointmentDto>(appointmentDomain);
+
+            return Ok(appointmentDto);
+        }
+    }
+}
